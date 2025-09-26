a230f9dc1591dfd256e9b98363fd0cb06a267044
QueenProtea <yizuzelbautista@gmail.com>
2025-09-26 11:39:56 -0600

    features : se agrego clase produto a modelo de servidor

--- ServidorInventarioPlus/Modelo/Producto.cs
+++ ServidorInventarioPlus/Modelo/Producto.cs
@@ -1,6 +1,17 @@
 ﻿namespace DefaultNamespace;
 
-public class Producto
-{
-    
-}
+ public class Producto
+    {
+        public int ProductoID { get; set; }
+        public string Codigo { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+        public int Stock { get; set; }
+        public int StockApartado { get; set; }
+        public int StockMinimo { get; set; }
+
+        // Relaciones
+        public ICollection<ProductoAtributo>? Atributos { get; set; }
+        public ICollection<Movimiento>? Movimientos { get; set; }
+        public ICollection<Reserva>? Reservas { get; set; }
+    }
